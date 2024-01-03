ee3496a69640dd37763de67328561291a15318a0
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-03 13:00:52 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Caroline Knight and is open-sourced on{" "}
+        <a href="https://github.com/carolineflora95/final-react-app-weather">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
