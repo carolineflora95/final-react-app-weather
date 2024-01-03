2375994cbc04558be7faed3d5debd1f0bf5bf858
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-03 13:37:03 +0100

    Added Bootstrap npm

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Caroline Knight and is open-sourced on{" "}
-        <a href="https://github.com/carolineflora95/final-react-app-weather">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="www.linkedin.com/in/caroline-flora-knight-48a24b118">
+            Caroline Knight{" "}
+          </a>
+          and is open-sourced on{" "}
+          <a href="https://github.com/carolineflora95/final-react-app-weather">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
