4b714790387128cbf668ab67eef96cd23bd15a21
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-03 13:57:17 +0100

    Added Weather Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 
 import "./App.css";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="www.linkedin.com/in/caroline-flora-knight-48a24b118">
