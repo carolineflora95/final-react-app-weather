0e8b8e27bdca05162e82ed84810556602cde2102
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-09 17:49:37 +0100

    added load function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
-import ForecastShow from "./ForecastShow";
 import axios from "axios";
+import ForecastShow from "./ForecastShow";
 
 export default function WeatherForecast(props) {
   let [done, setDone] = useState(false);
@@ -16,6 +16,12 @@ export default function WeatherForecast(props) {
     setDone(true);
   }
 
+  function load() {
+    let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (done) {
     return (
       <div className="WeatherForecast">
@@ -27,15 +33,15 @@ export default function WeatherForecast(props) {
                   <ForecastShow data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
